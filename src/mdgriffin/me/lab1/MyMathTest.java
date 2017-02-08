7164871c74f64201b46d8a08d256a64d7da0a7f6
Michael Griffin <mdgriffin064@gmail.com>
2017-02-08 21:19:48 +0000

    Reversed assert argument order to correct order

--- src/mdgriffin/me/lab1/MyMathTest.java
+++ src/mdgriffin/me/lab1/MyMathTest.java
@@ -7,29 +7,29 @@ import static org.junit.Assert.*;
 public class MyMathTest {
     @Test
     public void getmaxOfThree() throws Exception {
-        assertEquals(MyMath.getMaxOfThree(1,2,3), 3);
-        assertEquals(MyMath.getMaxOfThree(42, 23, 101), 101);
+        assertEquals(3, MyMath.getMaxOfThree(1,2,3));
+        assertEquals(101, MyMath.getMaxOfThree(42, 23, 101));
     }
 
     @Test
     public void calcPower() throws Exception {
-        assertEquals(MyMath.calcPower(4, 0), 1, 3);
-        assertEquals(MyMath.calcPower(8, 1), 8, 3);
-        assertEquals(MyMath.calcPower(2, 4), 16, 3);
-        assertEquals(MyMath.calcPower(3.5, 7), 6433.929, 3);
+        assertEquals(1, MyMath.calcPower(4, 0), 3);
+        assertEquals(8, MyMath.calcPower(8, 1), 3);
+        assertEquals(16, MyMath.calcPower(2, 4), 3);
+        assertEquals(6433.929, MyMath.calcPower(3.5, 7), 3);
     }
 
     @Test
     public void sumToN() throws Exception {
-        assertEquals(MyMath.sumToN(5), 15);
-        assertEquals(MyMath.sumToN(11), 66);
-        assertEquals(MyMath.sumToN(25), 325);
+        assertEquals(15, MyMath.sumToN(5));
+        assertEquals(66, MyMath.sumToN(11));
+        assertEquals(325, MyMath.sumToN(25));
     }
 
     @Test
     public void calcFactorial() throws Exception {
-        assertEquals(MyMath.calcFactorial(6), 720, 3);
-        assertEquals(MyMath.calcFactorial(11), 39916800, 3);
+        assertEquals(720, MyMath.calcFactorial(6), 3);
+        assertEquals(39916800, MyMath.calcFactorial(11), 3);
     }
 
     @Test
@@ -37,22 +37,22 @@ public class MyMathTest {
         int[] primeNums = new int[]{2, 3, 5, 7, 11, 13, 17, 59, 61, 97};
 
         for (int i =0; i < primeNums.length; i++) {
-            assertEquals(MyMath.isPrime(primeNums[i]), true);
+            assertEquals(true, MyMath.isPrime(primeNums[i]));
         }
 
         int[] notPrimeNums = new int[]{4, 6, 8, 8, 16, 18, 20, 21, 22};
 
         for (int j = 0; j < notPrimeNums.length; j++) {
-            assertEquals(MyMath.isPrime(notPrimeNums[j]), false);
+            assertEquals(false, MyMath.isPrime(notPrimeNums[j]));
         }
     }
 
     @Test
     public void isValidPassword() throws Exception {
-        assertEquals(MyMath.isValidPassword("Ab1cdefg"), true);
-        assertEquals(MyMath.isValidPassword("shorty"), false);
-        assertEquals(MyMath.isValidPassword("abcdefghij"), false);
-        assertEquals(MyMath.isValidPassword("123456789"), false);
+        assertEquals(true, MyMath.isValidPassword("Ab1cdefg"));
+        assertEquals(false, MyMath.isValidPassword("shorty"));
+        assertEquals(false, MyMath.isValidPassword("abcdefghij"));
+        assertEquals(false, MyMath.isValidPassword("123456789"));
     }
 
 }
