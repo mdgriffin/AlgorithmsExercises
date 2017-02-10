73e7888d8b01d0e4c08e9a922c6bf7944eed308a
Michael Griffin <mdgriffin064@gmail.com>
2017-02-10 12:35:00 +0000

    Adding JavaDoc comments to MyMath class

--- src/mdgriffin/me/lab1/MyMath.java
+++ src/mdgriffin/me/lab1/MyMath.java
@@ -1,6 +1,19 @@
 package mdgriffin.me.lab1;
 
+/**
+ * A Utility class of Math Methods
+ */
+
 public class MyMath {
+
+    /**
+     * Gets the largest of three numbers
+     *
+     * @param num1  The first comparison number
+     * @param num2  The second comparison number
+     * @param num3  The third comparison number
+     * @return      The largest number of the three
+     */
 
     public static int getMaxOfThree (int num1, int num2, int num3) {
         int max = num1;
@@ -13,6 +26,14 @@ public class MyMath {
 
         return max;
     }
+
+    /**
+     * Calculates number to a power
+     *
+     * @param num     The number to calculate
+     * @param power   The power
+     * @return        The number raised to the power
+     */
 
     public static double calcPower (double num, int power) {
         double sum = 1;
@@ -28,6 +49,13 @@ public class MyMath {
         return sum;
     }
 
+    /**
+     * Sums a range of numbers from 1 to n
+     *
+     * @param num  The upper limit of the range
+     * @return     The sum of the range
+     */
+
     public static int sumToN(int num) {
         int sum = 0;
 
@@ -41,6 +69,13 @@ public class MyMath {
 
         return sum;
     }
+
+    /**
+     * Calculates the factorial of a number
+     *
+     * @param num   The number to calculate the factorial of
+     * @return      The factorial of the supplied number
+     */
 
     public static double calcFactorial (int num) {
         double sum = 1;
@@ -56,6 +91,13 @@ public class MyMath {
         return sum;
     }
 
+    /**
+     * Check is a number is a prime number
+     *
+     * @param num The number to check
+     * @return    A boolean representing whethere the number is prime or not
+     */
+
     public static boolean isPrime (int num) {
         boolean isPrime = true;
         int range = num / 2;
@@ -70,6 +112,13 @@ public class MyMath {
 
         return isPrime;
     }
+
+    /**
+     * Checks a String to verify that it matches the valid password criteria
+     *
+     * @param pass The password String to check
+     * @return     A boolean representing whether the password is valid or not
+     */
 
     public static boolean isValidPassword (String pass) {
         boolean valid = true;
