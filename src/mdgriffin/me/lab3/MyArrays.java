9e8b9916fe105e604e45ed07c6da3cf6008abe51
Michael Griffin <mdgriffin064@gmail.com>
2017-02-14 21:31:50 +0000

    Added method headers for Array utility methods

---
+++ src/mdgriffin/me/lab3/MyArrays.java
@@ -0,0 +1,28 @@
+package mdgriffin.me.lab3;
+
+
+public class MyArrays {
+
+
+    public static int sum (int[] arr) {
+        return 1;
+    }
+
+    public static int findMax (int[] arr) {
+        return 1;
+    }
+
+    public static int findMin (int[] arr) {
+        return 1;
+    }
+
+    public static void remove (int[] arr, int pos) {
+
+    }
+
+    public static void insert(int[] arr, int el, int pos) {
+
+    }
+
+
+}
