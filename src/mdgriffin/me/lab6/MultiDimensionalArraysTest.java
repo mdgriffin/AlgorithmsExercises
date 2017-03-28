4afe6092fad4aec067f5c610da35e28fd449f3ad
Michael Griffin <mdgriffin064@gmail.com>
2017-03-28 16:41:57 +0100

    Added comments and one extra test array

--- src/mdgriffin/me/lab6/MultiDimensionalArraysTest.java
+++ src/mdgriffin/me/lab6/MultiDimensionalArraysTest.java
@@ -38,6 +38,7 @@ public class MultiDimensionalArraysTest {
 
     @Test
     public void isMagicSquare () throws  Exception {
+        // This array is a magic square
         int[][] arr = {
             {16, 3, 2, 13},
             {5, 10, 11, 8},
@@ -45,11 +46,20 @@ public class MultiDimensionalArraysTest {
             {4, 15, 14, 1}
         };
 
+        // This array does not contain whole 1..15 sequence
         int[][] arr2 =  {
             {3, 16, 2, 13},
             {5, 9, 11, 8},
             {9, 6, 7, 12},
             {4, 15, 14, 1}
+        };
+
+        // This array does contains whole 1..15 sequence but cols and rows do not sum
+        int[][] arr3 =  {
+                {3, 16, 2, 13},
+                {5, 10, 11, 8},
+                {9, 6, 7, 12},
+                {4, 15, 14, 1}
         };
 
         assertEquals(true, MultiDimensionalArrays.isMagicSquare(arr, 4, 4));
