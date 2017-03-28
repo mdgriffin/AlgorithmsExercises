2fb7bc7f08ebd0f80684d5560c94e1e919ffb5e6
Michael Griffin <mdgriffin064@gmail.com>
2017-03-28 15:17:52 +0100

    Added ClassDetails class

---
+++ src/mdgriffin/me/lab6/ClassDetails.java
@@ -0,0 +1,28 @@
+package mdgriffin.me.lab6;
+
+public class ClassDetails {
+
+    private String name;
+    private String roomNumber;
+
+    public ClassDetails(String name, String roomNumber) {
+        setName(name);
+        setRoomNumber(roomNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+}
