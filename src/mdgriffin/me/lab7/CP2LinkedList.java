064b2c32f3892fa39b5efffcfacbba5f6e578f2e
Michael Griffin <mdgriffin064@gmail.com>
2017-04-07 11:40:33 +0100

    Added driver class for LinkedList implementation

---
+++ src/mdgriffin/me/lab7/CP2LinkedList.java
@@ -0,0 +1,62 @@
+package mdgriffin.me.lab7;
+
+import java.util.NoSuchElementException;
+//singly linked list with first reference only
+
+/**
+ A linked list is a sequence of links with efficient
+ element insertion and removal. This class
+ contains a subset of the methods of the standard
+ java.util.LinkedList class.
+ */
+public class CP2LinkedList<E> {
+    private class Node {
+        public E data;
+        public Node next;
+    }
+    private Node first;
+
+    /**
+     Constructs an empty linked list.
+     */
+    public CP2LinkedList()
+    {
+        first = null;
+    }
+
+    /**
+     Adds an element to the front of the linked list.
+     @param element the element to add
+     */
+    public void addFirst(E element)
+    {
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = first;
+        first = newNode;
+    }
+
+    public E getFirst() {
+        if (first == null)
+            throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public void removeFirst() {
+        if (first == null)
+            throw new NoSuchElementException();
+        first = first.next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void print() {
+        Node current = first;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
---
+++ src/mdgriffin/me/lab7/CP2LinkedListDriver.java
@@ -0,0 +1,22 @@
+package mdgriffin.me.lab7;
+
+import java.util.Arrays;
+
+public class CP2LinkedListDriver {
+
+    public static void main(String[] args) {
+        CP2LinkedList<String> cp2LinkedList = new CP2LinkedList<>();
+
+
+        cp2LinkedList.addFirst("John");
+
+        //cp2LinkedList
+
+        /*System.out.println(Arrays.toString(cp2LinkedList));*/
+
+        cp2LinkedList.print();
+
+
+    }
+
+}
